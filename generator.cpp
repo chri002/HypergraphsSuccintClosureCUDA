b7a2813755f4af15c69d5dc3cdb3e63b90b0d6b0
Bolletta Christian <56335168+chri002@users.noreply.github.com>
2022-12-01 11:49:28 +0100

    fix correct

--- generator.cpp
+++ generator.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <algorithm>
 #include <fstream>
+#include <math.h>
 
 
 int main(int argn, char ** argvs){
@@ -22,7 +23,9 @@ int main(int argn, char ** argvs){
 	int n_v=25,m=0,n_e=n_v/2,n_s=n_v/3, to=0, m1,c, ms, remove=0;
 	std::string str="", strp="";
 	bool line=false, cicle=false, sphere=false;
+	int conto_totale = 0, conto_temp=0, lina=0;
 	
+	system("cls");
 	for(int i=0;i<argn-1; i++){
 		str = argvs[i];
 		strp = argvs[i+1];
@@ -40,10 +43,12 @@ int main(int argn, char ** argvs){
 			sphere = true;
 		if(str.substr(0,2)=="-r")
 			remove = std::atoi(argvs[i+1]);
-		if(str.substr(0,3)=="--h")
-			printf("Generator of hypergraph\n  generator.exe <option>\n\nOptions:\n\t-v <number of vertices>\n\t-e <number of edges>\n\t-s <number of supersets>\n\t-l -v <n.v.>: line graphs\n\t-l -c -v <n.v.>: line graphs cyclic\n\t-o -v <n.v.>: complete graphs\n\t-r <n.v.>: remove n.v. vertices");
+		if(str.substr(0,3)=="--h" || strp.substr(0,3)=="--h"){
+			printf("Generator of hypergraph\n  generator.exe <option>\n\nOptions:\n\t-v <number of vertices>\n\t-e <number of edges>\n\t-s <number of supersets>\n\t-r <n.v.>: remove n.v. vertices\nOptions complete:\n\t-l -v <n.v.>: line graphs\n\t-l -c -v <n.v.>: line graphs cyclic\n\t-o -v <n.v.>: complete graphs");
+			return 0;
+		}
 	}
-	
+
 	if(line){
 		n_e = n_v -1 + (cicle? 1:0);
 		n_s = n_v -1 + (cicle? 1:0);
@@ -55,11 +60,12 @@ int main(int argn, char ** argvs){
 	n_e -= remove;
 	
     
-	const int NS = n_s;
-	printf("Inizio creazione grafo (%d %d %d)\n", n_v, n_e, n_s);
+	printf("Inizio creazione grafo (%d %d %d)", n_v, n_e, n_s);
+		
+	conto_totale = n_v+n_e+n_s;
 	
 	std::ofstream myFile;
-	std::string name_file = (!line? (sphere? "grafo_sphere_" : "grafo_") : "grafo_line_" ) + std::to_string(n_v) +"_"+ std::to_string(n_e) +".txt";
+	std::string name_file = (!line? (sphere? "grafo_sphere_" : "grafo_") : "grafo_line_" ) + std::to_string(n_v) +"_"+ std::to_string(n_e)+"_"+std::to_string(n_s)+"o.txt";
 	myFile.open(name_file.c_str());
 	myFile << "INI " << n_v <<"," << n_e <<"\n";
 	
@@ -103,19 +109,33 @@ int main(int argn, char ** argvs){
 				//printf("create superset %d: %s:%d\n",i,str.c_str(),c);
 				i++;
 			}
+			conto_temp = i;
+			if((i%int(n_s/10))==0)
+				printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
 		}
 	else if(line)
 		for(int i=0; i<n_s; i++){
 			superset[i] = new int[]{i};
+			conto_temp = i;
+			if(i%(n_s/10)==0)
+				printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
 		}
-	else if(sphere)
+	else if(sphere){
 		superset[0] = new int[]{0};
-	
-	printf("write node\n");
+		printf("\033[%d;0H\t\t\t\t\t\t\t\t 1/%d",lina,conto_totale);
+	}
+	lina+=1;
+	printf("\nwrite node");
 	
 	for(int i=0; i<n_v; i++){
 		myFile <<  "(VE " << i <<")\n";
+		conto_temp = n_s+i;
+		if(i%(n_v/10)==0)
+			printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		
 	}
+	lina+=1;
+	printf("\nnode write");
 	
 	int *remList, tempo=0, k=0;
 	remList = (int*) malloc(sizeof(int)*remove);
@@ -137,6 +157,9 @@ int main(int argn, char ** argvs){
 	
 	for(int i=0; i<remove; i++) printf(" %d ",remList[i]);
 	
+	lina+=1;
+	printf("\nedge write");
+	
 	if(!line && !sphere)
 		for(int i=0; i<n_e; i++){
 			if((k<remove && remList[k]!=i) || k>=remove){
@@ -150,36 +173,56 @@ int main(int argn, char ** argvs){
 				}
 				myFile << "}," << superset[m][0] << "," << to << ")\n";
 			}else k++;
-			
+			conto_temp = n_v+n_s+i;
+			if(i%(n_e/10)==0)
+				printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		
 			
 		}
 	else if(line){
 		
 		if(cicle){
-			for(int i=0; i<n_s-1; i++)
+			for(int i=0; i<n_s-1; i++){
 				if((k<remove && remList[k]!=i) || k>=remove){
 					myFile<<"(HA {"<<i<<"},1,"<<i+1<<")\n";
 				}else k++;
+				conto_temp = n_v+n_s+i;
+				if(i%(n_s/10)==0)
+					printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		
+			}
 			myFile<<"(HA {"<<n_s-1<<"},1,0)\n";
-			
+			conto_temp++;;
+			printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		
 		}else{
 			for(int i=0; i<n_s; i++){
 				if((k<remove && remList[k]!=i) || k>=remove){
 					myFile<<"(HA {"<<i<<"},1,"<<i+1<<")\n";
 				}else k++;
+				conto_temp = n_v+n_s+i;
+				if(i%(n_s/10)==0)
+					printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		
 			}
 		}
 	}else if(sphere){
 		
-		for(int i=0; i<n_v; i++)
+		for(int i=0; i<n_v; i++){
 			if((k<remove && remList[k]!=i) || k>=remove){
 				for(int j=0; j<n_v; j++)
 					if(i!=j)
 						myFile<<"(HA {"<<i<<"},1,"<<j<<")\n";
 			}else k++;
-			
+			conto_temp = n_v+n_s+i;
+			if(i%(n_v/10)==0)
+				printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_temp,conto_totale);
+		}
 	}
+	
 	myFile.close();
 	
+	printf("\033[%d;0H\t\t\t\t\t\t\t\t %d/%d",lina,conto_totale,conto_totale);
+
 	
 }
