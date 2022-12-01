52da6279b49eefb09bb88887e042697a63ad12c8
Bolletta Christian <56335168+chri002@users.noreply.github.com>
2022-12-01 14:13:27 +0100

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -8,6 +8,8 @@ mkdir eseguibile
 )
 nvcc .\generator.cpp -o .\eseguibile\generator.exe
 nvcc -rdc=true -D FILE_OUT -D DEBUG  -D MAX_THREADS=1024 -D MAX_BLOCKS_A=1 -D MAX_BLOCKS_AI=1 -D NTHR=1 -D MAX_BLOCKS_B=16 -D TIME -std=c++17 -lineinfo -Xcompiler -openmp -Xlinker /HEAP:0x8096 .\progetto_G.cu -D HIDE -o .\eseguibile\progetto_1_1_16.exe
+nvcc -rdc=true -D FILE_OUT -D DEBUG  -D CPU -D NTHR=1 -D MAX_BLOCKS_B=16 -D TIME -std=c++17 -lineinfo -Xcompiler -openmp -Xlinker /HEAP:0x8096 .\progetto_G.cu -D HIDE -o .\eseguibile\progetto_16_CPU.exe
+nvcc -rdc=true -D FILE_OUT -D DEBUG  -D DYNAMIC -D NTHR=8 -D MAX_BLOCKS_B=16 -D TIME -std=c++17 -lineinfo -Xcompiler -openmp -Xlinker /HEAP:0x8096 .\progetto_G.cu -D HIDE -o .\eseguibile\progetto_16_DYNO.exe
 for /l %%a in (2, 1, 6) do ( 
 for /l %%b in (1, 1, 5) do ( 
 nvcc -rdc=true -D FILE_OUT -D DEBUG  -D MAX_THREADS=1024 -D MAX_BLOCKS_A=%%a -D MAX_BLOCKS_AI=%%b -D MAX_BLOCKS_B=16 -D TIME -std=c++17 -lineinfo -Xcompiler -openmp -Xlinker /HEAP:0x8096 .\progetto_G.cu -D HIDE -o .\eseguibile\progetto_%%a_%%b_16.exe
